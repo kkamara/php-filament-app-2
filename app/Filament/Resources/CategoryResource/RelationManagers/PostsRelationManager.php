1c54b7f72b97d1d609c90df9b4843c9e32587ab5
kkamara <kamaracomputers@gmail.com>
2024-10-01 19:40:15 +0100

    Add Relationship Manager (1-1 & 1-M)

--- app/Filament/Resources/CategoryResource/RelationManagers/PostsRelationManager.php
+++ app/Filament/Resources/CategoryResource/RelationManagers/PostsRelationManager.php
@@ -9,6 +9,20 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\CheckboxColumn;
+use Filament\Tables\Columns\ColorColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 
 class PostsRelationManager extends RelationManager
 {
@@ -17,11 +31,42 @@ class PostsRelationManager extends RelationManager
     public function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Forms\Components\TextInput::make('title')
-                    ->required()
-                    ->maxLength(255),
-            ]);
+        ->schema([
+            Section::make("Create a Post")
+                ->description("Create posts over here.")
+                // ->aside()
+                ->collapsible()
+                ->schema([
+                    TextInput::make("title")->rules("min:3|max:10")->required(), // ->rules([]) can accept an array
+                        // ->in(["test", "hello"])
+                        // ->minLength(3) for text inputs
+                        // ->maxLength(10) for text inputs
+                        // ->numeric() for numbers
+                        // ->minValue(3) for numbers
+                        // ->maxValue(10) for numbers
+                    TextInput::make("slug")->unique(ignoreRecord: true)->required(),
+                    // Omitting category because Filament will automatically select that,
+                    // being on the category resource.
+                    ColorPicker::make("color")->required(),
+
+                    MarkdownEditor::make("content")->required()->columnSpan("full"), // or ->columnSpanFull()
+                ])->columnSpan(2)->columns(2),
+            Group::make()->schema([
+                Section::make("Image")->collapsible()->schema([
+                    FileUpload::make("thumbnail")->disk("public")->directory("thumbnails"),
+                ])->columnSpan(span: 1),
+                Section::make("Meta")->collapsible()->schema([
+                    TagsInput::make("tags")->required(),
+                    Checkbox::make("published"),
+                ]),
+            ]),
+        ])->columns([
+            // Tailwind responsive sizes
+            "default" => 1,
+            "md" => 2,
+            "lg" => 3,
+            "xl" => 4,
+        ]);
     }
 
     public function table(Table $table): Table
@@ -30,6 +75,8 @@ class PostsRelationManager extends RelationManager
             ->recordTitleAttribute('title')
             ->columns([
                 Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('slug'),
+                Tables\Columns\CheckboxColumn::make('published'),
             ])
             ->filters([
                 //
